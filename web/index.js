e3ab48d8fee4cdf433963a43a3e7cfca82d6133c
Laptop1/Ninad P.Chaudhari <nchaudhari@albany.edu>
2018-03-23 21:15:46 -0400

    first commit

---
+++ web/index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
+app.get('/', function(req, res){
+res.sendFile(__dirname + '/index.html');
+});
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+for(i = 0 ; i < 10; i ++){
+io.emit('clap',"something");
+}
+});
